1c2f644442791b9b5003fbef9ba58486ba352bc7
Pedro Miguel Leal de Carvalho <tunafishmessiah@gmail.com>
2016-05-24 15:48:44 +0100

    FirstNode
    Dune

---
+++ BanaConspiracyIII/GameScene.swift
@@ -0,0 +1,20 @@
+//
+//  GameScene.swift
+//  BanaConspiracyIII
+//
+//  Created by a9632 - Pedro Carvalho on 24/05/16.
+//  Copyright (c) 2016 a9632 - Pedro Carvalho. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMoveToView(view: SKView) {
+    }
+    
+    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+    }
+   
+    override func update(currentTime: CFTimeInterval) {
+    }
+}
