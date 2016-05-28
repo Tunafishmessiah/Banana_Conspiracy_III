334cbf90c1b3517c0c6a593c6a1dc5948b8992bd
Pedro Miguel Leal de Carvalho <tunafishmessiah@gmail.com>
2016-05-28 23:46:04 +0100

    Enemy has more stuff to him
    See comments but small details

--- BanaConspiracyIII/Enemy.swift
+++ BanaConspiracyIII/Enemy.swift
@@ -14,7 +14,7 @@ class Enemy : Pawn
     
     var EnemySprites : [SKTexture] = []
     var attackTimer : Int = 0
-    func Enemy()
+    func Enemy( screenSize : CGPoint, rightScreenSide : Bool)
     {
         //They will be walking always, soo we'll just have 2 sprites of him walking
         EnemySprites.append(SKTexture(imageNamed: "ICUP00"))//right leg front
@@ -22,6 +22,16 @@ class Enemy : Pawn
         EnemySprites.append(SKTexture(imageNamed: "ICUP02"))//attack
         
         self.Pawn("ICUP", CGPointMake(1,1),true,1)
+        
+        //como as imagens estão todas viradas para a direita
+        
+        if(rightScreenSide)
+        {
+            //virar o men para o outro lado
+            self.Node.xScale = -1
+        }
+        
+        
     }
     
     public func Update(playerPos : CGPoint)
@@ -36,7 +46,7 @@ class Enemy : Pawn
         
         if(dist < self.Node.size.width) //a value that i think it's right, maybe self.size.width/2 + player.size.width/2 wouldn't be bad too
         {
-            self.Node.texture = enemySprites[2]
+            self.Node.texture = EnemySprites[2]
             self.attackTimer = 20
             //to make him attack
         }
